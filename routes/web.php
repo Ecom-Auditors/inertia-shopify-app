c452b241ef5953f3291b1fa4bce4b3affcc42aff
wnateg <wnateg@gmail.com>
2023-09-23 13:04:36 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use EcomAuditors\InertiaShopifyApp\Http\Controllers\AuthController;
+use EcomAuditors\InertiaShopifyApp\Http\Controllers\BillingController;
 use EcomAuditors\InertiaShopifyApp\Http\Controllers\UserController;
 use EcomAuditors\InertiaShopifyApp\Http\Controllers\WebhookController;
 use EcomAuditors\InertiaShopifyApp\Http\Middleware\HandleAppBridge;
@@ -11,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['web', ProtectIframe::class])->group(function () {
     Route::get('auth/callback', [AuthController::class, 'callback'])->name('auth.callback');
     Route::get('auth/token', [AuthController::class, 'token'])->name('auth.token');
+
+    Route::get('billing/return', [BillingController::class, 'return'])->name('billing.return');
 
     Route::middleware(HandleAppBridge::class)->group(function () {
         Route::get('/user/profile', [UserController::class, 'show'])->name('user.show');
